e2f947026bc8cb60765133a02e20c5ddc885003c
Simon Pham <simon@hasbrain.com>
2018-08-26 10:27:17 +0700

    Show main activity after splash screen

--- app/src/main/java/com/example/home/demo/SplashScreenActivity.kt
+++ app/src/main/java/com/example/home/demo/SplashScreenActivity.kt
@@ -1,12 +1,22 @@
 package com.example.home.demo
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.home.demo.ui.MainActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity( Intent(this@SplashScreenActivity, MainActivity::class.java),
+                    ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+            finish()
+        }, 500)
     }
 }
